841080475cd8fb0bb9503a90759af5aa2345dc01
NoumanBaloch <mrnoumanbaloch@gmail.com>
2019-04-24 06:26:50 -0700

    Registration Form using MVC

---
+++ UserRegistration/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            // Fetching the Yes, No properties fro WorkinngDetails table
+            MyDBEntities db = new MyDBEntities();
+            List<WorkingDetail> list = db.WorkingDetails.ToList();
+            ViewBag.WorkingDetail = new SelectList(list, "IsWorkingID", "IsWorking");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SaveRecord(UserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            else
+            {
+                try
+                {
+                    //Storing the forms values into User table of Database
+                    MyDBEntities db = new MyDBEntities();
+                    User userObj = new User
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Email,
+                        PhoneNo = model.PhoneNo,
+                        IsWorkingID = model.IsWorkingID
+                    };
+                    db.Users.Add(userObj);
+                    db.SaveChanges();
+
+                    //This function is use to send the email to the user
+                    MailMessage mm = new MailMessage("mrnoumanbaloch@gmail.com", model.Email)
+                    {
+                        Subject = "Email from MTBC",
+                        Body = "Hello dear " + model.FirstName + Environment.NewLine + "Many thanks for your order." + Environment.NewLine + "We will deliver the goods in less than 48 hours." + Environment.NewLine + "Kind Regards," + Environment.NewLine + "Jennifer" + Environment.NewLine + Environment.NewLine + "Please visit our website"
+                    };
+                    ;
+                    mm.IsBodyHtml = false;
+                    SmtpClient smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true
+                    };
+                    NetworkCredential nc = new NetworkCredential("mrnoumanbaloch@gmail.com", "Password");
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = nc;
+                    smtp.Send(mm);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistration/Models/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserRegistration.Models
+{
+    public class UserViewModel
+    {
+       
+        public int ID { get; set; }
+        [Required]
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage = "Must contain only letters.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage = "Must contain only letters.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Enter a valid email address.")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}", ErrorMessage = "Enter a valid phone number.")]
+        public string PhoneNo { get; set; }
+    
+       [Required]
+       public int IsWorkingID { get; set; }
+
+
+    }
+}
